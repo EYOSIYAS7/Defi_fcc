feccf2aa341655219eaff9ae0c7a8c2b82d896e0
EYOSIYAS <eyosiyasmengesha@gmail.com>
2025-03-21 12:33:02 +0300

    setup for defiProject

---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("hardhat-deploy");
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28",
+      },
+      {
+        version: "0.4.19",
+      },
+    ],
+  },
+
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
+};
